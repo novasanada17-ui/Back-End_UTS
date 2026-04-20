3437b73724be56ebbbf0944fc4cff02fc638a50d
novasanada17-ui <nova.sanada@students.mikroskil.ac.id>
2026-04-20 21:24:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import express from "express";
 import mysql2 from "mysql2";
-import dotenv from "dotenv";
-dotenv.config();
+
 
 let app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let db = mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "localhost",
+    user: "mahasigma",
+    password: "abc",
+    database: "keretaapi_db"
 });
 
 db.connect((err) => {
